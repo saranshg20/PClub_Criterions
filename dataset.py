9131eaac887b27b2aedd2e9a9d1c72a2a694c6e2
Saransh Shivhare <77162374+saranshg20@users.noreply.github.com>
2021-08-19 09:34:48 +0530

    Updating values in dataset.py

--- dataset.py
+++ dataset.py
@@ -9,11 +9,11 @@ def lr_dataset_generator(slope, n, std_dev):
     
     
 def lr_dataset():
-    x, y = lr_dataset_generator(10, 100, 150)
-    x_train = x[:80]
-    y_train = y[:80]
+    x, y = lr_dataset_generator(10, 200, 100)
+    x_train = x[:(0.8*len(x))]
+    y_train = y[:(0.8*len(x))]
 
-    x_test = x[80:]
-    y_test = y[80:]
+    x_test = x[(0.8*len(x)):]
+    y_test = y[(0.8*len(x)):]
     
     return x_train, y_train, x_test, y_test
